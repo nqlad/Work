d5ca1960cd1541f880c83323be9a3a47a64452c2
nqlad <nqlad77@yandex.ru>
2018-07-30 17:01:56 +0300

    The DependencyInjection Component

--- public/index.php
+++ public/index.php
@@ -3,25 +3,19 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
-//header("Content-Type: application/json; charset=UTF-8");
+use \Symfony\Component\DependencyInjection\ContainerBuilder;
+use \Symfony\Component\Config\FileLocator;
+use \Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 require(__DIR__ . '/../vendor/autoload.php');
 
-$deserializer       = new \App\Serialization\JsonSerializer();
-$validator          = new \App\Validation\Validator();
-$persister          = new \App\Database\PostgresDriver('pgsql:host=localhost;port=5432;dbname=postgres','postgres','yfNL4W');
-$requestFactory     = new \App\Http\RequestFactory();
-$responseFactory    = new \App\Http\ResponseFactory($deserializer);
-$responseSender     = new \App\Http\ResponseSender();
-$routeParser        = new \App\RequestHandler\RouteParser();
+$container  = new ContainerBuilder();
+$loader     = new YamlFileLoader($container, new FileLocator(__DIR__.'/../config'));
+$loader->load('services.yaml');
 
-$getNote            = new \App\Action\GetNoteAction($persister,$responseFactory,$validator);
-$getNoteCollection  = new \App\Action\GetNoteCollectionAction($persister,$responseFactory);
-$deleteNote         = new \App\Action\DeleteNoteAction($deserializer,$persister,$responseFactory,$validator);
-$postNote           = new \App\Action\PostNoteAction($deserializer,$persister,$responseFactory,$validator);
-$putNote            = new \App\Action\UpdateNoteAction($deserializer,$validator,$persister,$responseFactory);
-
-$router = new \App\RequestHandler\RoutingHandler($responseFactory, $getNoteCollection,$getNote,$deleteNote,$postNote,$putNote,$routeParser);
+$requestFactory = $container->get('requestFactory');
+$router         = $container->get('routingHandler');
+$responseSender = $container->get('responseSender');
 
 $kernel = new \App\Kernel($requestFactory, $router, $responseSender);
 $kernel->run();
